c31557072b760ecfd14834d90733c9e5e99d7e02
chulwoo jeong <chulwoorulz2002@hotmail.com>
2016-11-09 17:32:18 +0900

    spring day two work

---
+++ BankSystemSpring/src/main/java/com/cjon/bank/utility/DBTemplate.java
@@ -0,0 +1,54 @@
+package com.cjon.bank.utility;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBTemplate {
+	
+	private Connection con;
+	
+	public DBTemplate() {
+		try {
+			//1. jdbc driver loading(mysql용)
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			//2. DB 접속
+			String url="jdbc:mysql://localhost:3306/library"; //jdbc url
+			String id="teddy";
+			String pw="teddy";
+			con=DriverManager.getConnection(url, id, pw);
+			
+			con.setAutoCommit(false);
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	public void commit(){
+		try {
+			con.commit();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void rollback(){
+		try {
+			con.rollback();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Connection getCon() {
+		return con;
+	}
+
+	public void setCon(Connection con) {
+		this.con = con;
+	}
+	
+}
+
